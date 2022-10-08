d6637a683da3f930d5aadf0f05b1222bbd9ddb61
Andrew Way <andrew@wayfamily.net>
2022-10-08 10:37:39 -0700

    fix script

--- configmybox.sh
+++ configmybox.sh
@@ -2,8 +2,8 @@
 
 # install zsh 
 # check if zsh is installed if not install
-if ! command -v <the_command> &> /dev/null
-then
+if ! command -v zsh &> /dev/null 
+then  
   sudo apt-get update && sudo apt-get install -y zsh
   chsh -s /bin/zsh
 fi
@@ -15,8 +15,10 @@ git clone --depth=1 https://github.com/mattmc3/antidote.git ${ZDOTDIR:-~}/.antid
 curl -sS https://starship.rs/install.sh | sh
 
 # create symlinks
-ln -s ./.zshrc ~/.zshrc
+ln -s $(pwd)/.zshrc ~/.zshrc
 mkdir -p ~/.config
-ln -s ./.config/starship.toml ~/.config/starship.toml
-ln -s ./.vimrc ~/.vimrc
-ln -s ./.zsh_plugins.txt ~/.zsh_plugins.txt
+ln -s $(pwd)/.config/starship.toml ~/.config/starship.toml
+ln -s $(pwd)/.vimrc ~/.vimrc
+ln -s $(pwd)/.zsh_plugins.txt ~/.zsh_plugins.txt
+
+source ~/.zshrc
