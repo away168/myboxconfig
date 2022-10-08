92ba70b6bd9f8709694e786e27c85383adefb38b
Andrew Way <andrew@wayfamily.net>
2022-10-07 19:41:38 -0700

    helper script

---
+++ configmybox.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# install zsh 
+# check if zsh is installed if not install
+if ! command -v <the_command> &> /dev/null
+then
+  sudo apt-get update && sudo apt-get install -y zsh
+  chsh -s /bin/zsh
+fi
+
+# install antidote (zsh plugin manager)
+git clone --depth=1 https://github.com/mattmc3/antidote.git ${ZDOTDIR:-~}/.antidote
+
+#install starship
+curl -sS https://starship.rs/install.sh | sh
+
+# create symlinks
+ln -s ./.zshrc ~/.zshrc
+ln -s ./.config/starship.toml ~/.config/starship.toml
+ln -s ./.vimrc ~/.vimrc
+ln -s ./.zsh_plugins.txt ~/.zsh_plugins.txt
