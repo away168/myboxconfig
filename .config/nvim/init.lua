59da90e70a323b693690f2cff16756b35a743308
Andrew Way <andrew@wayfamily.net>
2024-09-22 22:51:15 -0700

    init lazy plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,20 +1,2 @@
---[[ init.lua ]]
-
--- LEADER
--- These keybindings need to be defined before the first /
--- is called; otherwise, it will default to "\"
--- https://mattermost.com/blog/turning-neovim-into-a-full-fledged-code-editor-with-lua/
---
-vim.g.mapleader = ","
-vim.g.localleader = "\\"
-
--- IMPORTS
-require('vars')      -- Variables
-require('opts')      -- Options
-require('keys')      -- Keymaps
-require('plug')      -- Plugins
-
-require'nvim-tree'.setup {}
-require('lualine').setup {
-  options = { theme  = 'dracula'}
-}
+require("away.core")
+require("away.lazy")
--- .config/nvim/lua/vars.lua
+++
@@ -1,8 +0,0 @@
---[[ vars.lua ]]
-
-local g = vim.g
-g.t_co = 256
-g.background = "dark"
-
-local packer_path = vim.fn.stdpath('config') .. '/site'
-vim.o.packpath = vim.o.packpath .. ',' .. packer_path
