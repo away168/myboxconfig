135946d84d73f68d82df55101091b7322f3ed708
Andrew Way <andrew.way@env0.com>
2024-09-26 15:55:50 -0700

    spacing

--- .config/nvim/lua/away/plugins/todo-comments.lua
+++ .config/nvim/lua/away/plugins/todo-comments.lua
@@ -1,21 +1,21 @@
 return {
-  "folke/todo-comments.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    local todo_comments = require("todo-comments")
-    
-    -- set keymaps
-    local keymap = vim.keymap -- for conciseness
+	"folke/todo-comments.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local todo_comments = require("todo-comments")
 
-    keymap.set("n", "]t", function()
-      todo_comments.jump_next()
-    end, { desc = "Next todo comment" })
+		-- set keymaps
+		local keymap = vim.keymap -- for conciseness
 
-    keymap.set("n", "[t", function()
-      todo_comments.jump_prev()
-    end, { desc = "Previous todo comment" })
+		keymap.set("n", "]t", function()
+			todo_comments.jump_next()
+		end, { desc = "Next todo comment" })
 
-    todo_comments.setup()
-  end,
+		keymap.set("n", "[t", function()
+			todo_comments.jump_prev()
+		end, { desc = "Previous todo comment" })
+
+		todo_comments.setup()
+	end,
 }
